09e657fc5adc49eea8a4002167c65e01af6c929f
DaniloNunezN <dan.nunezn@duocuc.cl>
2026-06-01 19:44:03 -0400

    Conexion

--- api-suministros/src/main/java/cl/duoc/api_suministros/ApiSuministrosApplication.java
+++ api-suministros/src/main/java/cl/duoc/api_suministros/ApiSuministrosApplication.java
@@ -2,6 +2,8 @@ package cl.duoc.api_suministros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApiSuministrosApplication {
@@ -10,4 +12,8 @@ public class ApiSuministrosApplication {
 		SpringApplication.run(ApiSuministrosApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
