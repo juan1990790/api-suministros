b9f0e7209a565c146c096dc8fdae6d44bc6e52b6
DaniloNunezN <dan.nunezn@duocuc.cl>
2026-06-01 19:38:26 -0400

    Repository

--- api-suministros/src/main/java/cl/duoc/api_suministros/repository/suministroRepository.java
+++ api-suministros/src/main/java/cl/duoc/api_suministros/repository/suministroRepository.java
@@ -1,7 +1,6 @@
 package cl.duoc.api_suministros.repository;
 
 import cl.duoc.api_suministros.model.suministroModel;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,11 +9,5 @@ import java.util.Optional;
 @Repository
 public interface suministroRepository extends JpaRepository<suministroModel, Long> {
 
-    // Buscar por ID de fabricante para la validación externa y stock
-    @Transactional
     Optional<suministroModel> findByIdFabricante(String idFabricante);
-
-    // Eliminar por ID de fabricante (opcional, por si no quieres usar el ID numérico)
-    @Transactional
-    void deleteByIdFabricante(String idFabricante);
 }
