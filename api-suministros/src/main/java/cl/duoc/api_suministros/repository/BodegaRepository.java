6b60c45e1d223105a29e3cbd784b0ed82fa6fa95
DaniloNunezN <dan.nunezn@duocuc.cl>
2026-06-01 19:22:40 -0400

    Cambio de reglas de modelo, y DTO

--- api-suministros/src/main/java/cl/duoc/api_suministros/repository/BodegaRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.duoc.api_suministros.repository;
-
-import cl.duoc.api_suministros.model.Bodega;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BodegaRepository extends JpaRepository<Bodega, Long> {
-}
