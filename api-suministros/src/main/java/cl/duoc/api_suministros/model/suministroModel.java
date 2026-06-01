4db16510a34d129b3ab4cbef71573d7ffc07e81d
DaniloNunezN <dan.nunezn@duocuc.cl>
2026-06-01 19:28:10 -0400

    Cambio de reglas de modelo, y DTO

--- api-suministros/src/main/java/cl/duoc/api_suministros/model/suministroModel.java
+++ api-suministros/src/main/java/cl/duoc/api_suministros/model/suministroModel.java
@@ -2,6 +2,49 @@ package cl.duoc.api_suministros.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "componentes")
+public class suministroModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String idFabricante;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    // Validación del Caso 2: Jamás negativos
+    @NotNull
+    @Min(0)
+    @Column(nullable = false)
+    private Integer unidades;
+
+    // Validación del Caso 2: Jamás negativos
+    @NotNull
+    @Min(0)
+    @Column(nullable = false)
+    private Double valorComercial;
+
+    @Column(nullable = false)
+    private String estado;
+
+    private String sku;
+
+    // Campo dinámico: Se muestra en Postman, pero no se guarda en la BD
+    @Transient
+    private String infoProveedorRemoto;
+}
+package cl.duoc.api_suministros.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
