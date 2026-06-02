3d1061dc71581695bebc8ccb2555596cf0ec0073
juan1990790 <ju.opazoh@duocuc.cl>
2026-06-01 20:08:05 -0400

    Update AppConfig.java
    conexion para patch

--- api-suministros/src/main/java/cl/duoc/api_suministros/config/AppConfig.java
+++ api-suministros/src/main/java/cl/duoc/api_suministros/config/AppConfig.java
@@ -1,4 +1,16 @@
 package cl.duoc.api_suministros.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        return new RestTemplate(requestFactory);
+    }
 }
