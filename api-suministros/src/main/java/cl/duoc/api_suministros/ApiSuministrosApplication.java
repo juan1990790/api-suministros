273b574d4c88b3d081a4e8e8557f7e5d6c0c8600
Byron Monsalve <by.monsalve@duocuc.cl>
2026-06-01 20:08:57 -0400

    Update ApiSuministrosApplication.java
    Cambio de estructura

--- api-suministros/src/main/java/cl/duoc/api_suministros/ApiSuministrosApplication.java
+++ api-suministros/src/main/java/cl/duoc/api_suministros/ApiSuministrosApplication.java
@@ -2,18 +2,11 @@ package cl.duoc.api_suministros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApiSuministrosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiSuministrosApplication.class, args);
-	}
-
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+    public static void main(String[] args) {
+       SpringApplication.run(ApiSuministrosApplication.class, args);
+    }
 }
