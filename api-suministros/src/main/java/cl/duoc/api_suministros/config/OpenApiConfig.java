6de0dabac34dae170d0f3a0bc0436b296583ab10
DaniloNunezN <dan.nunezn@duocuc.cl>
2026-06-26 21:00:04 -0400

    Codigo open api

--- api-suministros/src/main/java/cl/duoc/api_suministros/config/OpenApiConfig.java
+++ api-suministros/src/main/java/cl/duoc/api_suministros/config/OpenApiConfig.java
@@ -1,4 +1,14 @@
 package cl.duoc.api_suministros.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("API Suministros").version("1.0"));
+    }
 }
